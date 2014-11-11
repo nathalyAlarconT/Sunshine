a0bb6791c893bf5d1152da73f4a3d3f23cceb866
Nathaly Samsung Home <enat87@gmail.com>
2014-11-11 10:19:27 -0500

    Lesson2 - p2 - Adding a Menu Inside a Fragment
    The Refresh option also calls to the Async Task. At this point the App
    crushes

--- app/src/main/java/com/nathalyalarcont/sunshine/ForecastFragment.java
+++ app/src/main/java/com/nathalyalarcont/sunshine/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -26,6 +29,27 @@ import java.util.List;
     public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState){  //Este método ocurre antes del onCreateView
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true); //Indica que el fragmento tiene un menu
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
