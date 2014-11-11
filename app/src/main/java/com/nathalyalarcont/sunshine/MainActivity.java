64c2d8137de6f1a93ac135b0891a91c2f00b76c3
Nathaly Samsung Home <enat87@gmail.com>
2014-11-11 10:17:44 -0500

    Lesson2 - p1 - Async Task
    Adding ForecastFragment with Async Tasks

--- app/src/main/java/com/nathalyalarcont/sunshine/MainActivity.java
+++ app/src/main/java/com/nathalyalarcont/sunshine/MainActivity.java
@@ -1,19 +1,9 @@
 package com.nathalyalarcont.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 
@@ -29,7 +19,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -56,50 +46,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+ }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // Creating fake data
-                String[] forecastArray = {
-                        "Today - Sunny - 88/63",
-                        "Tomorrow - Foggy - 70//40",
-                        "Weds - Cloudy - 72/63",
-                        "Thurs - Asteroids - 75/65",
-                        "Fri - Heavy Rain - 65/75",
-                        "Sat - HELP TRAPPED IN WEAThERSTATION - 22/33",
-                        "Sun - Sunny - 80/68"
-                };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
-                    // The current context (this fragment's parameter activity)
-                    getActivity(),
-                    // ID of list item layout
-                    R.layout.list_item_forecast,
-                    // ID of the textView to populate
-                    R.id.list_item_forecast_textview,
-                    // Forecast data
-                    weekForecast);
-            // Get a reference to the ListView
-            // and attach this adapter to ListView
-            ListView listView = (ListView) rootView.findViewById(
-                    R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapter);
-
-
-
-            return rootView;
-        }
-    }
-}
