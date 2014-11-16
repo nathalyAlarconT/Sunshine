4edd16307450826bc2f00cb6bde1c320c78374df
Nathaly Samsung Home <enat87@gmail.com>
2014-11-15 22:36:55 -0500

    Lesson3 - p1 - Adding a Toast

--- app/src/main/java/com/nathalyalarcont/sunshine/ForecastFragment.java
+++ app/src/main/java/com/nathalyalarcont/sunshine/ForecastFragment.java
@@ -10,8 +10,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -92,7 +94,13 @@ import java.util.List;
         ListView listView = (ListView) rootView.findViewById(
                 R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+               String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
