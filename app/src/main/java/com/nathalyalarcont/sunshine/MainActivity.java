5112090fed776ba2b27f4d7ab1a32218710e909e
Nathaly Samsung Home <enat87@gmail.com>
2014-11-15 22:28:21 -0500

    Lesson3 - p6 - Adding a Map!

--- app/src/main/java/com/nathalyalarcont/sunshine/MainActivity.java
+++ app/src/main/java/com/nathalyalarcont/sunshine/MainActivity.java
@@ -5,14 +5,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
-
-
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.net.Uri;
+import android.util.Log;
 
 
 
 public class MainActivity extends ActionBarActivity {
 
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +48,34 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        if(id == R.id.action_map){
+            openPreferredLocationInMap();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap(){
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        );
+
+
+        Uri geoLocation = Uri.parse("geo: 0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else{
+            Log.d(LOG_TAG, "Couldn't call" + location + ", no exists");
+        }
     }
  }
 
